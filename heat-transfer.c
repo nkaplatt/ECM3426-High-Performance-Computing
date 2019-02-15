a91e0e4d1d46bc8fbc57c2462ed9fd5797f8cff7
nkaplatt <hello@nickplatt.co.uk>
2019-02-15 13:02:19 +0000

    job scheduler & heat transfer equation

--- heat-transfer.c
+++ heat-transfer.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #define axis 500
-int main () {
+#define timestep 0.0000009
+#define alphasqrd 0.01
+
+int main() {
+  // 2D array
   float tempValues[axis][axis];
 
+  // initial temperature landscape
   FILE *outfile;
   outfile=fopen("heat-transfer-init.dat","w");
-
   for (int i = 0; i < axis; i++) {
     for (int j = 0; j < axis; j++) {
       if (150 <= j && j <= 350 && 150 <= i && i <= 350) {
@@ -17,5 +21,30 @@ int main () {
     }
   }
   fclose(outfile);
+
+  // formula (4)
+  for(int x = 0; x < 10000; x++) {
+    for (int i = 1; i < axis; i++) {
+      for (int j = 1; j < axis; j++) {
+        float rateOfChange = alphasqrd * (
+          (tempValues[i+1][j] - 2 * tempValues[i][j] + tempValues[i-1][j]) / 0.0002 * 0.0002 + 
+          (tempValues[i][j+1] - 2 * tempValues[i][j] + tempValues[i][j-1]) / 0.0002 * 0.0002
+        );
+
+        // formular (5)
+        tempValues[i-1][j-1] = tempValues[i-1][j-1] + rateOfChange * timestep;
+      }
+    }
+  }
+
+  FILE *resultfile;
+  resultfile=fopen("heat-transfer-result.dat","w");
+  for (int i = 0; i < axis; i++) {
+    for (int j = 0; j < axis; j++) {
+      fprintf(resultfile,"%f %f %f\n", i * 0.0002, j * 0.0002, tempValues[i][j]);
+    }
+  }
+  fclose(resultfile);
+
   return 0;
 }
