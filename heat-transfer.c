3492e0b91b51cdc1717c7586bda0f9ee89ada9c8
nkaplatt <hello@nickplatt.co.uk>
2019-02-19 09:40:32 +0000

    working serial execution

--- heat-transfer.c
+++ heat-transfer.c
@@ -1,55 +1,58 @@
 #include <stdio.h>
-#define axis 500
-#define timestep 0.0000009
-#define alphasqrd 0.01
+#define AXISINTERVAL 500
+#define DELETATIME 0.0000009
+#define ALPHASQRD 0.01
+#define TIMESTEPS 10000
+#define DELTAXY 0.0002
 
 int main() {
   // 2D array
-  float tempValues[axis][axis];
+  double tempValues[AXISINTERVAL][AXISINTERVAL];
 
   // initial temperature landscape
   FILE *outfile;
   outfile=fopen("heat-transfer-init.dat","w");
   int i;
   int j;
-  for (i = 0; i < axis; i++) {
-    for (j = 0; j < axis; j++) {
+  for (i = 0; i < AXISINTERVAL; i++) {
+    for (j = 0; j < AXISINTERVAL; j++) {
       if (150 <= j && j <= 350 && 150 <= i && i <= 350) {
         tempValues[i][j] = 50.0;
       } else {
         tempValues[i][j] = 20.0;
       }
-      fprintf(outfile,"%f %f %f\n", i * 0.0002, j * 0.0002, tempValues[i][j]);
+      fprintf(outfile,"%f %f %f\n", i * DELTAXY, j * DELTAXY, tempValues[i][j]);
     }
   }
   fclose(outfile);
 
-  // formula (4)
+  FILE *testfile;
+  double rateOfChange[AXISINTERVAL][AXISINTERVAL];
   int x;
-  float rateOfChange[500][500];
-  for(x = 0; x < 10000; x++) {
-    for (i = 1; i < axis-1; i++) {
-      for (j = 1; j < axis-1; j++) {
-        rateOfChange[i][j] = alphasqrd * (
-          (tempValues[i+1][j] - 2 * tempValues[i][j] + tempValues[i-1][j]) / 0.0002 * 0.0002 + 
-          (tempValues[i][j+1] - 2 * tempValues[i][j] + tempValues[i][j-1]) / 0.0002 * 0.0002
+  for(x = 0; x < TIMESTEPS; x++) {
+    for (i = 1; i < AXISINTERVAL-1; i++) {
+      for (j = 1; j < AXISINTERVAL-1; j++) {
+        // formula (4)
+        rateOfChange[i-1][j-1] = ALPHASQRD * (
+          (tempValues[i+1][j] - 2 * tempValues[i][j] + tempValues[i-1][j]) / (DELTAXY * DELTAXY) + 
+          (tempValues[i][j+1] - 2 * tempValues[i][j] + tempValues[i][j-1]) / (DELTAXY * DELTAXY)
         );
       }
     }
 
-    for (i = 1; i < axis-1; i++) {
-      for (j = 1; j < axis-1; j++) {
+    for (i = 1; i < AXISINTERVAL; i++) {
+      for (j = 1; j < AXISINTERVAL; j++) {
         // formular (5)
-        tempValues[i][j] = tempValues[i][j] + rateOfChange[i][j] * timestep;
+        tempValues[i][j] += rateOfChange[i-1][j-1] * DELETATIME;
       }
-    }    
+    }
   }
 
   FILE *resultfile;
   resultfile=fopen("heat-transfer-result.dat","w");
-  for (i = 0; i < axis; i++) {
-    for (j = 0; j < axis; j++) {
-      fprintf(resultfile,"%f %f %f\n", i * 0.0002, j * 0.0002, tempValues[i][j]);
+  for (i = 0; i < AXISINTERVAL; i++) {
+    for (j = 0; j < AXISINTERVAL; j++) {
+      fprintf(resultfile,"%f %f %f\n", i * DELTAXY, j * DELTAXY, tempValues[i][j]);
     }
   }
   fclose(resultfile);
