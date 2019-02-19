94c9cb3018b99bc103cf0befe929fe3568f646f5
nkaplatt <hello@nickplatt.co.uk>
2019-02-19 11:52:40 +0000

    trial parallel loop

--- heat-transfer.c
+++ heat-transfer.c
@@ -9,22 +9,31 @@
 
 int main() {
   // 2D array
-  const AXISINTERVAL = 500;
-  const DELETATIME = 0.0000009;
-  const ALPHASQRD = 0.01;
-  const TIMESTEPS = 10000;
-  const DELTAXY = 0.0002;
+  const int AXISINTERVAL = 500;
+  const double DELETATIME = 0.0000009;
+  const float ALPHASQRD = 0.01;
+  const int TIMESTEPS = 10000;
+  const float DELTAXY = 0.0002;
   double tempValues[AXISINTERVAL][AXISINTERVAL];
+
+  omp_set_num_threads(2);
+  int sum = 0;
+  int b = 0;
+  #pragma omp parallel for default(none) shared(sum) private(b)
+  for (b = 0; b < 50; b++) {
+    sum += b;
+  }
+  printf("%i", sum);
 
   // initial temperature landscape
   FILE *outfile;
-  outfile=fopen("heat-transfer-init.dat","w");
   int i;
   int j;
-  #pragma omp parallel for default(none) shared(tempValues) private(i, j)
+  omp_set_num_threads(2);
+  #pragma omp parallel for default(none) shared(tempValues, outfile) private(i, j)
   for (i = 0; i < AXISINTERVAL; i++) {
+    printf("thread num %d", omp_get_thread_num());
     for (j = 0; j < AXISINTERVAL; j++) {
-      // printf("thread %d of %d", omp_get_thread_num(), omp_get_num_threads());
       if (150 <= j && j <= 350 && 150 <= i && i <= 350) {
         tempValues[i][j] = 50.0;
       } else {
@@ -35,36 +44,36 @@ int main() {
   }
   fclose(outfile);
 
-  FILE *testfile;
-  double rateOfChange[AXISINTERVAL][AXISINTERVAL];
-  int x;
-  for(x = 0; x < TIMESTEPS; x++) {
-    for (i = 1; i < AXISINTERVAL-1; i++) {
-      for (j = 1; j < AXISINTERVAL-1; j++) {
-        // formula (4)
-        rateOfChange[i-1][j-1] = ALPHASQRD * (
-          (tempValues[i+1][j] - 2 * tempValues[i][j] + tempValues[i-1][j]) / (DELTAXY * DELTAXY) + 
-          (tempValues[i][j+1] - 2 * tempValues[i][j] + tempValues[i][j-1]) / (DELTAXY * DELTAXY)
-        );
-      }
-    }
+  // FILE *testfile;
+  // double rateOfChange[AXISINTERVAL][AXISINTERVAL];
+  // int x;
+  // for(x = 0; x < TIMESTEPS; x++) {
+  //   for (i = 1; i < AXISINTERVAL-1; i++) {
+  //     for (j = 1; j < AXISINTERVAL-1; j++) {
+  //       // formula (4)
+  //       rateOfChange[i-1][j-1] = ALPHASQRD * (
+  //         (tempValues[i+1][j] - 2 * tempValues[i][j] + tempValues[i-1][j]) / (DELTAXY * DELTAXY) + 
+  //         (tempValues[i][j+1] - 2 * tempValues[i][j] + tempValues[i][j-1]) / (DELTAXY * DELTAXY)
+  //       );
+  //     }
+  //   }
 
-    for (i = 1; i < AXISINTERVAL; i++) {
-      for (j = 1; j < AXISINTERVAL; j++) {
-        // formular (5)
-        tempValues[i][j] += rateOfChange[i-1][j-1] * DELETATIME;
-      }
-    }
-  }
+  //   for (i = 1; i < AXISINTERVAL; i++) {
+  //     for (j = 1; j < AXISINTERVAL; j++) {
+  //       // formular (5)
+  //       tempValues[i][j] += rateOfChange[i-1][j-1] * DELETATIME;
+  //     }
+  //   }
+  // }
 
-  FILE *resultfile;
-  resultfile=fopen("heat-transfer-result.dat","w");
-  for (i = 0; i < AXISINTERVAL; i++) {
-    for (j = 0; j < AXISINTERVAL; j++) {
-      fprintf(resultfile,"%f %f %f\n", i * DELTAXY, j * DELTAXY, tempValues[i][j]);
-    }
-  }
-  fclose(resultfile);
+  // FILE *resultfile;
+  // resultfile=fopen("heat-transfer-result.dat","w");
+  // for (i = 0; i < AXISINTERVAL; i++) {
+  //   for (j = 0; j < AXISINTERVAL; j++) {
+  //     fprintf(resultfile,"%f %f %f\n", i * DELTAXY, j * DELTAXY, tempValues[i][j]);
+  //   }
+  // }
+  // fclose(resultfile);
 
   return 0;
 }
